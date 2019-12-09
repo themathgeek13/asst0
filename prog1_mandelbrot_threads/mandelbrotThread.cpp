3e79c540ab55346c0ecf2c1abe295f8aa9e89421
Rohan Rao <themathgeek13@gmail.com>
2019-12-09 13:12:59 -0500

    spatial decomposition for work allocation

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -36,7 +36,12 @@ void* workerThreadStart(void* threadArgs) {
     // of the output image.  For example, in a program that uses two
     // threads, thread 0 could compute the top half of the image and
     // thread 1 could compute the bottom half.
-
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1, 
+            args->width, args->height, 
+            args->threadId*args->height/args->numThreads, 
+            args->height/args->numThreads, 
+            args->maxIterations, args->output);
+    
     printf("Hello world from thread %d\n", args->threadId);
 
     return NULL;
